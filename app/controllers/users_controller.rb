5468ed71ca0b4c097b842d94a01caba79ad27183
Hiroaki Ito <pigaru.gooood.mar19@gmail.com>
2017-10-28 03:23:21 +0000

    10.28

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,8 +21,8 @@ class UsersController < ApplicationController
     end
   end
 
-  def edit
-  end
+   def edit
+   end
 
   def update
     if @user.update_attributes(user_params)
@@ -53,6 +53,6 @@ class UsersController < ApplicationController
     # 正しいユーザーかどうか確認
     def correct_user
       @user = User.find(params[:id])
-      redirect_to(root_url) unless @user == current_user
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
