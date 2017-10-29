2a59abc1ef0d6bb17a6ab77dd95d6ffdc7b66367
Hiroaki Ito <pigaru.gooood.mar19@gmail.com>
2017-10-29 06:40:48 +0000

    11.38

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,16 +15,16 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def create
+   def create
     @user = User.new(user_params)
     if @user.save
-      UserMailer.account_activation(@user).deliver_now
+      @user.send_activation_email
       flash[:info] = "Please check your email to activate your account."
       redirect_to root_url
     else
       render 'new'
     end
-  end
+   end
 
    def edit
    end
