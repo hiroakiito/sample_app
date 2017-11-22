77cb7f3196fa775190f5abf42b5ee5a71d16a6b9
Hiroaki Ito <pigaru.gooood.mar19@gmail.com>
2017-11-22 14:40:00 +0000

    micropostkensaku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @microposts = @user.microposts.paginate(page: params[:page])
+    # 検索拡張機能として.search(params[:search])を追加    
+    @microposts = @user.microposts.paginate(page: params[:page]).search(params[:search])
   end
 
   def new
